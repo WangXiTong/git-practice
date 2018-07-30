eef3fc214986afbf599830ccc16637589071e602
wangxitong <wangxitong@baidu.com>
2018-07-30 09:56:05 +0800

    webpack exercise

---
+++ src/math.js
@@ -0,0 +1,11 @@
+/**
+ * @file LmListPage
+ * @author wangxitong(wangxitong@baidu.com)
+ */
+export function square(x) {
+    return x * x;
+}
+
+export function cube(x) {
+    return x * x * x;
+}
---
+++ src/print.js
@@ -0,0 +1,12 @@
+/**
+ * string
+ * @file LmListPage
+ * @author wangxitong(wangxitong@baidu.com)
+ */
+export default function printMe() {
+    // console.log('I get called from print.js');
+    // console.log('I geasdfasdfadsf');
+    console.log('Updating print.js...');
+    console.log('qwewrqwer');
+    console.log('qwewrqwer');
+}
